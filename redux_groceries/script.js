b5d55472dfe360d6ecab04997d9b6ea736e2b124
kajal panchal <kajalpanchal@kajals-Air.home>
2024-06-22 11:56:57 -0700

    finish html

---
+++ redux_groceries/script.js
@@ -0,0 +1,9 @@
+// Establish DOM elements as variables
+const grocerySubmit = document.getElementById('addGrocery')
+const list = document.getElementById('list')
+const clearBtn = document.getElementById('clear')
+
+// Instantiate default state value:
+const initialState = {
+    groceries: []
+}
